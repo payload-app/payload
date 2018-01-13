dda726f49d728a11decce2baf8982b8927eab4dc
Harrison Harnisch <hharnisc@gmail.com>
2018-01-13 15:33:44 -0600

    Add Status Broadcaster service (#2)
    * initialize status-broadcaster app
    
    * implement status broadcaster rpc endpoint stub
    
    * add status-broadcaster to docker-compose development environment
    
    * implement github status broadcaster
    
    * add request library
    
    * set User Agent to context

---
+++ status-broadcaster/index.js
@@ -0,0 +1,45 @@
+const rp = require('request-promise')
+const {
+  rpc,
+  method
+} = require('@hharnisc/micro-rpc')
+const {
+  router,
+  get,
+  post,
+} = require('microrouter')
+
+const rpcHandler = rpc(
+  method('broadcastStatus', ({
+    accessToken,
+    owner,
+    repo,
+    sha,
+    state,
+    description,
+    targetUrl,
+    context,
+  }) => rp({
+    method: 'POST',
+    uri: `https://api.github.com/repos/${owner}/${repo}/statuses/${sha}?access_token=${accessToken}`,
+    headers: {
+      'User-Agent': context,
+    },
+    json: true,
+    body: {
+      state,
+      description,
+      target_url: targetUrl,
+      context,
+    }
+  })),
+)
+
+const healthHandler = () => ({
+  status: 'OK',
+})
+
+module.exports = router(
+  get('/healthz', healthHandler),
+  post('/rpc', rpcHandler),
+)
