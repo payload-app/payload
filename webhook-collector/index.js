cb37d3d8071db128bad0fb8f0f521dfcb71da4cf
Harrison Harnisch <hharnisc@gmail.com>
2018-01-14 21:01:17 -0600

    Stub in webhook-collector service (#3)
    * add webhook-collector service (debug print pushed hooks)
    
    * update dev status-broadcaster port
    
    * update dev backend port
    
    * update dev frontend port

---
+++ webhook-collector/index.js
@@ -0,0 +1,24 @@
+const { json } = require('micro')
+const {
+  router,
+  get,
+  post,
+} = require('microrouter')
+
+const healthHandler = () => ({
+  status: 'OK',
+})
+
+const webhookHandler = async (req, res) => {
+  const payload = await json(req)
+  console.log('*******')
+  console.log('Event:', req.headers['x-github-event'])
+  console.log(JSON.stringify(payload, null, 2))
+  console.log('*******')
+  return 'OK'
+}
+
+module.exports = router(
+  get('/healthz', healthHandler),
+  post('/', webhookHandler),
+)
