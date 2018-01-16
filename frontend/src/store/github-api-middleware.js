271a9764ee569b3d1a9140b66d69d92eb525dca2
case <case@casesandberg.com>
2018-01-15 21:50:05 -0800

    Github Api Middleware

---
+++ frontend/src/store/github-api-middleware.js
@@ -0,0 +1,62 @@
+// @flow
+import request from 'request'
+
+export const GITHUB_API = 'GITHUB_API'
+
+export const status = {
+  REQUEST: 'request',
+  SUCCESS: 'success',
+  FAILURE: 'failure',
+}
+
+type Middleware = () => any => any => any
+
+type Config = {
+  baseUrl?: string,
+  apiKey?: string,
+}
+
+export type GithubAction = {
+  method?: 'GET',
+  type: 'string',
+  endpoint: 'string',
+  transform?: any => any,
+}
+
+export const middleware = ({
+  baseUrl = 'https://api.github.com/',
+}: Config = {}) => {
+  return (() => next => action => {
+    const apiAction = action[GITHUB_API]
+    if (apiAction) {
+      const {
+        method = 'GET',
+        type,
+        endpoint,
+        transform = body => ({ body }),
+      }: GithubAction = apiAction
+
+      next({ type, status: status.REQUEST })
+
+      request(
+        {
+          method,
+          url: `${baseUrl}${endpoint}`,
+        },
+        (error, response, body) => {
+          if (error) {
+            next({ type, error, status: status.FAILURE })
+          } else {
+            next({
+              type,
+              ...transform(JSON.parse(body)),
+              status: status.SUCCESS,
+            })
+          }
+        },
+      )
+    } else {
+      next(action)
+    }
+  }: Middleware)
+}
