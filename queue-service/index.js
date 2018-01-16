b60477661d28aa86106be748a72418f1a62ab5d4
Harrison Harnisch <hharnisc@gmail.com>
2018-01-16 11:04:00 -0600

    WIP: hash an item and use as lease key

--- queue-service/index.js
+++ queue-service/index.js
@@ -1,4 +1,5 @@
 const Redis = require('ioredis')
+const { sha256 } = require('js-sha256')
 const {
   rpc,
   method
@@ -15,10 +16,53 @@ const redis = new Redis({
   port: 6379
 })
 
+const leasedKey = ({
+  queue,
+  item,
+}) => {
+  const itemKey =
+    sha256
+      .create()
+      .update(item)
+      .hex()
+  return `${queue}:leased:${itemKey}`
+}
+
+const processingQueue = ({
+  queue
+}) =>
+  `${queue}:processing`
+
+const workerName = () => process.env.WORKER_NAME || 'worker'
+
 const rpcHandler = rpc(
-  method('createTask', ({ queue, task }) =>
-    redis.lpush(queue, JSON.stringify(task))),
-  method('processTask', () => 'OK'),
+  method('createTask', ({
+    queue,
+    task,
+    retries=0,
+    timeout=60, // seconds
+  }) =>
+    redis.lpush(queue, JSON.stringify({
+      task,
+      retries,
+      timeout,
+    }))),
+  method('processTask', async ({
+    queue,
+  }) => {
+    const item = await redis.rpoplpush(
+      queue,
+      processingQueue({ queue })
+    )
+    if (item) {
+      const { timeout } = JSON.parse(item)
+      redis.setex(leasedKey({ queue, item }), timeout, workerName())
+    }
+    // TODO: think about if the id should be returned for completeing and failing tasks
+    // TODO: think about the format of the lease key
+    return JSON.parse(item)
+  }),
+  method('failTask', () => 'OK'),
   method('completeTask', () => 'OK'),
 )
 
