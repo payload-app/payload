dfe7a3136a0f8896a61189eb0848ff1f2eb5f15e
Harrison Harnisch <hharnisc@gmail.com>
2018-01-23 10:16:32 -0600

    implement createUser endpoint

--- user-service/createUser.js
+++ user-service/createUser.js
@@ -1,1 +1,48 @@
-module.exports = ({ collectionClient }) => async () => 'OK'
+const Joi = require('joi')
+const { validate, parseValidationErrorMessage } = require('./utils')
+const { createError } = require('@hharnisc/micro-rpc')
+
+const schema = Joi.object().keys({
+  avatar: Joi.string().required(),
+  username: Joi.string().required(),
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  accessToken: Joi.string().required(),
+  email: Joi.string().required(),
+  type: Joi.string().required(),
+})
+
+module.exports = ({ collectionClient }) => async ({
+  avatar,
+  username,
+  firstName,
+  lastName,
+  accessToken,
+  email,
+  type = 'github',
+}) => {
+  const user = {
+    avatar,
+    username,
+    firstName,
+    lastName,
+    accessToken,
+    email,
+    type,
+  }
+  try {
+    const validation = await validate({
+      value: user,
+      schema,
+    })
+  } catch (error) {
+    throw createError({
+      message: parseValidationErrorMessage({ error }),
+    })
+  }
+
+  const { insertedId } = await collectionClient.insertOne(user)
+  return {
+    id: insertedId,
+  }
+}
