7ae9c9204b8c39b3ca834ae38c1bd55d0693c3f8
Harrison Harnisch <hharnisc@gmail.com>
2018-01-26 09:59:44 -0600

    implement getSession endpoint

--- session-service/index.js
+++ session-service/index.js
@@ -1,8 +1,12 @@
 const { rpc, method } = require('@hharnisc/micro-rpc')
 const { router, get, post } = require('microrouter')
 const createSession = require('./createSession')
+const getSession = require('./getSession')
 
-const rpcHandler = rpc(method('createSession', createSession))
+const rpcHandler = rpc(
+  method('createSession', createSession),
+  method('getSession', getSession),
+)
 
 const healthHandler = () => 'OK'
 
