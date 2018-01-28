398424979389970f73a03f0402b6b6ea8c2c8a0d
Harrison Harnisch <hharnisc@gmail.com>
2018-01-28 10:27:30 -0600

    enqueue work from pull request

--- webhook-collector/index.js
+++ webhook-collector/index.js
@@ -1,16 +1,58 @@
 const { json } = require('micro')
 const { router, get, post } = require('microrouter')
+const RPCClient = require('@hharnisc/micro-rpc-client')
+
+const queueServiceClient = new RPCClient({
+  url: 'http://queue-service:3000/rpc',
+})
 
 const healthHandler = () => ({
   status: 'OK',
 })
 
-const webhookHandler = async (req, res) => {
+const enqueuePullRequest = async ({ payload }) => {
+  const action = payload.action
+  // ignore actions that aren't PR synchronize or opened
+  if (!['opened', 'synchronize'].includes(action)) {
+    return
+  }
+  const repository = payload.repository
+  const owner = repository.owner.login
+  const ownerType = repository.owner.type.toLowerCase()
+  const repo = repository.name
+
+  const pullRequest = payload.pull_request
+  const base = {
+    sha: pullRequest.base.sha,
+    branch: pullRequest.base.ref,
+  }
+  const head = {
+    sha: pullRequest.head.sha,
+    branch: pullRequest.head.ref,
+  }
+  const task = {
+    owner,
+    repo,
+    base,
+    head,
+    ownerType,
+    type: 'github',
+  }
+  const { taskId } = await queueServiceClient.call('createTask', {
+    queue: process.env.WORKER_QUEUE,
+    task,
+    retries: 0,
+    lease: 60,
+  })
+  console.log(`Enqueued task with id: ${taskId}`)
+}
+
+const webhookHandler = async req => {
+  const event = req.headers['x-github-event']
   const payload = await json(req)
-  console.log('*******')
-  console.log('Event:', req.headers['x-github-event'])
-  console.log(JSON.stringify(payload, null, 2))
-  console.log('*******')
+  if (event === 'pull_request') {
+    await enqueuePullRequest({ payload })
+  }
   return 'OK'
 }
 
