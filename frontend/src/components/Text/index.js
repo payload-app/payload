16dbca3c2162bba6b0651f85016e7a5cc12a8b44
Harrison Harnisch <hharnisc@gmail.com>
2018-02-10 14:19:38 -0600

    use a default text color

--- frontend/src/components/Text/index.js
+++ frontend/src/components/Text/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { fontFamily } from '../style/font'
+import { text } from '../style/color'
 
-export default ({ size = 1, color = '#333333', children }) => (
+export default ({ size = 1, color = text, children }) => (
   <span style={{ fontFamily, fontSize: `${size}rem`, color }}>{children}</span>
 )
