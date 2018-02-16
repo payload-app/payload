e17e5a2fb54cd4f11908a3cb7ef72f6de79bad11
Harrison Harnisch <hharnisc@gmail.com>
2018-02-16 10:34:47 -0600

    remove unused layout

--- frontend/src/layout/index.js
+++
@@ -1,6 +0,0 @@
-export { actionTypes, actions } from './actions'
-export { reducer, storeKey, selectors } from './store'
-
-export { default as Header } from './Header'
-export { default as Layout } from './Layout'
-export { default as Page } from './Page'
