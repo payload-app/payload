59fe9e9c729101b9f48a2bea912cb488bd306c22
Harrison Harnisch <hharnisc@gmail.com>
2018-02-17 12:02:20 -0600

    add not active flag RepoListItem

--- frontend/src/RepoList/components/RepoListItem/index.js
+++ frontend/src/RepoList/components/RepoListItem/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import { Text } from '../../../components'
+import { red } from '../../../components/style/color'
+
+const ActiveText = () => (
+  <Text capitalize={true} background={red}>
+    Not Active
+  </Text>
+)
 
 export default ({ repo }) => (
-  <Text>
-    {repo.owner}/{repo.repo}
-  </Text>
+  <div>
+    <Text size={1.5}>
+      {repo.owner}/{repo.repo}
+    </Text>
+    <div>{repo.active ? null : <ActiveText active={repo.active} />}</div>
+  </div>
 )
--- frontend/src/components/Text/index.js
+++ frontend/src/components/Text/index.js
@@ -2,12 +2,19 @@ import React from 'react'
 import { fontFamily } from '../style/font'
 import { text } from '../style/color'
 
-export default ({ size = 1, color = text, capitalize, children }) => (
+export default ({
+  size = 1,
+  color = text,
+  background,
+  capitalize,
+  children,
+}) => (
   <span
     style={{
       fontFamily,
       fontSize: `${size}rem`,
       color,
+      background,
       textTransform: capitalize ? 'uppercase' : undefined,
     }}
   >
