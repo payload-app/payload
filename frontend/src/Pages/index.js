77b523c54abdb588ee8a865850237dd73f0c3661
Harrison Harnisch <hharnisc@gmail.com>
2018-02-20 10:16:55 -0600

    complete todo

--- frontend/src/Pages/index.js
+++ frontend/src/Pages/index.js
@@ -7,10 +7,6 @@ import List from '../PageList'
 import Loading from '../PageLoading'
 import Run from '../PageRun'
 
-// TODO: impelement routing scheme
-// :type/:ownerType/:ownerName/:repoName/:sha
-// github/organization/payload-app/org-webhook-tester/D5293EF1B027E68B60
-//                     ^selector  ^ repo run details ^ one run
 export default () => (
   <Switch>
     <ProtectedRoute exact path={baseRoute()} component={Loading} />
