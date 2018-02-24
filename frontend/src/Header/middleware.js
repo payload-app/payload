aee41eb950046934b0cd2b3fb0ae5ef95ff78fe6
Harrison Harnisch <hharnisc@gmail.com>
2018-02-23 22:29:10 -0600

    move header title and subtitle setting in the Header middleware

--- frontend/src/Header/middleware.js
+++ frontend/src/Header/middleware.js
@@ -3,13 +3,34 @@ import { selector as repoListSelector } from '../RepoList'
 import { actionTypes as routingActionTypes, routes } from '../Routing'
 import { actionTypes as dataFetchActionTypes } from '@hharnisc/async-data-fetch'
 
+const setSubtitleTrackingCount = ({ dispatch, getState }) => {
+  const count = getState()[repoListSelector].repos.reduce((count, repo) => {
+    if (repo.active) {
+      return count + 1
+    }
+    return count
+  }, 0)
+  dispatch(
+    headerActions.setSubtitle({
+      subtitle: `Tracking ${count} Repositor${count === 1 ? 'y' : 'ies'}...`,
+    }),
+  )
+}
+
 export default ({ dispatch, getState }) => next => action => {
   next(action)
   switch (action.type) {
     case routingActionTypes.EMIT:
       if (action.route === routes.REPO_LIST) {
-        dispatch(headerActions.setTitle({ title: 'Payload Dashboard' }))
-        dispatch(headerActions.setSubtitle({ subtitle: 'Loading...' }))
+        if (getState()[repoListSelector].repos.length) {
+          setSubtitleTrackingCount({
+            dispatch,
+            getState,
+          })
+        } else {
+          dispatch(headerActions.setTitle({ title: 'Payload Dashboard' }))
+          dispatch(headerActions.setSubtitle({ subtitle: 'Loading...' }))
+        }
       }
       break
     case `repos_${dataFetchActionTypes.FETCH_START}`:
@@ -18,23 +39,27 @@ export default ({ dispatch, getState }) => next => action => {
     case `activateRepo_${dataFetchActionTypes.FETCH_SUCCESS}`:
     case `activateRepo_${dataFetchActionTypes.FETCH_FAIL}`:
     case `repos_${dataFetchActionTypes.FETCH_SUCCESS}`:
-      const count = getState()[repoListSelector].repos.reduce((count, repo) => {
-        if (repo.active) {
-          return count + 1
-        }
-        return count
-      }, 0)
-      dispatch(
-        headerActions.setSubtitle({
-          subtitle: `Tracking ${count} Repositor${
-            count === 1 ? 'y' : 'ies'
-          }...`,
-        }),
-      )
+      setSubtitleTrackingCount({
+        dispatch,
+        getState,
+      })
       break
     case `activateRepo_${dataFetchActionTypes.FETCH_START}`:
       dispatch(
         headerActions.setSubtitle({ subtitle: 'Activating Repository...' }),
+      )
+      break
+    case `getRun_${dataFetchActionTypes.FETCH_SUCCESS}`:
+      const { owner, repo, branch, sha } = action.result
+      dispatch(headerActions.setTitle({ title: `${owner}/${repo}` }))
+      dispatch(headerActions.setSubtitle({ subtitle: `${branch} » ${sha}` }))
+      break
+    case `getRun_${dataFetchActionTypes.FETCH_START}`:
+      dispatch(headerActions.setTitle({ title: 'Loading Run...' }))
+      dispatch(
+        headerActions.setSubtitle({
+          subtitle: 'Data Collection In Progress',
+        }),
       )
       break
     default:
--- frontend/src/PageRun/middleware.js
+++ frontend/src/PageRun/middleware.js
@@ -3,7 +3,6 @@ import {
   actionTypes as dataFetchActionTypes,
 } from '@hharnisc/async-data-fetch'
 import { actionTypes as routingActionTypes, routes } from '../Routing'
-import { actions as headerActionTypes } from '../Header'
 
 export default ({ dispatch }) => next => action => {
   next(action)
@@ -21,20 +20,8 @@ export default ({ dispatch }) => next => action => {
             },
           }),
         )
-        dispatch(headerActionTypes.setTitle({ title: 'Loading Run...' }))
-        dispatch(
-          headerActionTypes.setSubtitle({
-            subtitle: 'Data Collection In Progress',
-          }),
-        )
       }
       break
-    case `getRun_${dataFetchActionTypes.FETCH_SUCCESS}`:
-      const { owner, repo, branch, sha } = action.result
-      dispatch(headerActionTypes.setTitle({ title: `${owner}/${repo}` }))
-      dispatch(
-        headerActionTypes.setSubtitle({ subtitle: `${branch} » ${sha}` }),
-      )
     default:
       break
   }
