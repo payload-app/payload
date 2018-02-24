1f5a85524979e02d893b75bf6045757226e5a709
Harrison Harnisch <hharnisc@gmail.com>
2018-02-23 22:16:43 -0600

    remove debug log

--- frontend/src/Routing/reducer.js
+++ frontend/src/Routing/reducer.js
@@ -11,7 +11,6 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.EMIT:
-      console.log('action', action)
       return {
         ...state,
         route: action.route,
