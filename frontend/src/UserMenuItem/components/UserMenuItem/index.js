4ba15708d46d41c5c69adf35140a2d55cf53c905
Harrison Harnisch <hharnisc@gmail.com>
2018-03-06 09:57:16 -0600

    implement basic layout for UserMenuItem

--- frontend/src/UserMenuItem/components/UserMenuItem/index.js
+++ frontend/src/UserMenuItem/components/UserMenuItem/index.js
@@ -1,3 +1,24 @@
 import React from 'react'
+import { Image, Text } from '../../../components'
 
-export default () => <div>UserMenuItem</div>
+const getAvatar = ({ user }) => user.accounts.github.avatar
+const getName = ({ user }) => user.accounts.github.name
+
+export default ({ user }) => (
+  <div
+    style={{
+      display: 'flex',
+      alignItems: 'center',
+    }}
+  >
+    <div style={{ flexGrow: 1 }}>
+      <Text weight={400}>{getName({ user })}</Text>
+    </div>
+    <Image
+      height={'4rem'}
+      width={'4rem'}
+      circle={true}
+      src={getAvatar({ user })}
+    />
+  </div>
+)
--- frontend/src/UserMenuItem/components/UserMenuItem/story.js
+++ frontend/src/UserMenuItem/components/UserMenuItem/story.js
@@ -2,4 +2,24 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import UserMenuItem from './'
 
-storiesOf('UserMenuItem', module).add('default', () => <UserMenuItem />)
+const user = {
+  _id: '5a9aecd5d788d2001e95cbec',
+  email: 'hharnisc@gmail.com',
+  accounts: {
+    github: {
+      avatar: 'https://avatars0.githubusercontent.com/u/1388079?v=4',
+      username: 'hharnisc',
+      name: 'Harrison Harnisch',
+    },
+  },
+  organizationIds: [
+    '5a9aecd997cd03001faff186',
+    '5a9aecd997cd03001faff187',
+    '5a9aecd997cd03001faff188',
+    '5a9aecd997cd03001faff189',
+  ],
+}
+
+storiesOf('UserMenuItem', module).add('default', () => (
+  <UserMenuItem user={user} />
+))
