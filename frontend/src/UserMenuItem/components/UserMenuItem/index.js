828c9a85e88f675c25739fc527c6fcc6b6637950
Harrison Harnisch <hharnisc@gmail.com>
2018-03-09 10:00:55 -0600

    cleanup UserMenuItem with PopoverContainer component

--- frontend/src/UserMenuItem/components/UserMenuItem/index.js
+++ frontend/src/UserMenuItem/components/UserMenuItem/index.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {
   Image,
   Text,
-  Popover,
   Link,
   HR,
-  Overlay,
   Button,
+  PopoverContainer,
 } from '../../../components'
 import { text } from '../../../components/style/color'
 
@@ -14,80 +13,49 @@ const getAvatar = ({ user }) => user.accounts.github.avatar
 const getName = ({ user }) => user.accounts.github.name
 const getEmail = ({ user }) => user.email
 
-export default class UserMenuItem extends Component {
-  constructor() {
-    super()
-    this.state = {
-      popoverVisible: false,
-    }
-  }
+const PopoverComponent = ({ user }) => (
+  <div
+    style={{
+      border: `1px solid ${text}`,
+      padding: '1rem',
+    }}
+  >
+    <Text>{getEmail({ user })}</Text>
+    <HR />
+    <div>
+      <Link>Settings</Link>
+    </div>
+    <div>
+      <Link>Logout</Link>
+    </div>
+  </div>
+)
 
-  handleItemClick() {
-    this.setState({
-      popoverVisible: true,
-    })
-  }
-
-  handleOverlayClick() {
-    this.setState({
-      popoverVisible: false,
-    })
-  }
-
-  render() {
-    const { user } = this.props
-    const { popoverVisible } = this.state
-    return (
-      <div>
-        <div
-          style={{
-            position: 'relative',
-          }}
-        >
-          <Button
-            noStyle={true}
-            fillContainer={true}
-            onClick={() => this.handleItemClick()}
-          >
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-              }}
-            >
-              <div style={{ flexGrow: 1 }}>
-                <Text weight={400}>{getName({ user })}</Text>
-              </div>
-              <Image
-                height={'4rem'}
-                width={'4rem'}
-                circle={true}
-                src={getAvatar({ user })}
-              />
-            </div>
-          </Button>
-          <Popover hidden={!popoverVisible}>
-            <div
-              style={{
-                border: `1px solid ${text}`,
-                padding: '1rem',
-              }}
-            >
-              <Text>{getEmail({ user })}</Text>
-              <HR />
-              <div>
-                <Link>Settings</Link>
-              </div>
-              <div>
-                <Link>Logout</Link>
-              </div>
-            </div>
-          </Popover>
-        </div>
-        {popoverVisible ? (
-          <Overlay onClick={() => this.handleOverlayClick()} />
-        ) : null}
+const AnchorComponent = ({ user, onShowPopover }) => (
+  <Button noStyle={true} fillContainer={true} onClick={onShowPopover}>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      <div style={{ flexGrow: 1 }}>
+        <Text weight={400}>{getName({ user })}</Text>
       </div>
-    )
-  }
-}
+      <Image
+        height={'4rem'}
+        width={'4rem'}
+        circle={true}
+        src={getAvatar({ user })}
+      />
+    </div>
+  </Button>
+)
+
+export default ({ user }) => (
+  <PopoverContainer
+    PopoverComponent={PopoverComponent}
+    AnchorComponent={AnchorComponent}
+    user={user}
+  />
+)
