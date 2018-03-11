fd72f4c7d8bd3d955405b35ff1707799db57c24e
case <case@casesandberg.com>
2018-03-11 12:21:02 -0700

    Add Spacing

--- frontend/src/PageList/index.js
+++ frontend/src/PageList/index.js
@@ -4,13 +4,15 @@ import RepoSelector from '../RepoSelector'
 import RepoList from '../RepoList'
 
 export default () => (
-  <div>
+  <div style={{ padding: 60 }}>
     <Header />
-    <div>
-      <RepoSelector />
-    </div>
-    <div>
-      <RepoList />
+    <div style={{ display: 'flex', marginTop: 50 }}>
+      <div style={{ marginRight: 40 }}>
+        <RepoSelector />
+      </div>
+      <div style={{ flex: 1 }}>
+        <RepoList />
+      </div>
     </div>
   </div>
 )
