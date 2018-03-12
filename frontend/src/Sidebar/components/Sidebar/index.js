098fdb46fa7e4ef6bcb60488e16e8ff21c5b13dd
Harrison Harnisch <hharnisc@gmail.com>
2018-03-12 09:59:53 -0500

    add UserMenuItem to sidebar

--- frontend/src/Sidebar/components/Sidebar/index.js
+++ frontend/src/Sidebar/components/Sidebar/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SidebarItem from '../SidebarItem'
+import UserMenuItem from '../../../UserMenuItem'
 
 export default ({ items, onChange, value }) => {
   const handleSelect = ({ item }) => () => {
@@ -8,17 +9,20 @@ export default ({ items, onChange, value }) => {
     }
   }
   return (
-    <div>
-      {items.map(item => (
-        <div key={item.key} style={{ cursor: 'pointer' }}>
-          <SidebarItem
-            active={JSON.stringify(item) === JSON.stringify(value)}
-            onClick={handleSelect({ item })}
-          >
-            {item.display}
-          </SidebarItem>
-        </div>
-      ))}
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <div style={{ flexGrow: 1 }}>
+        {items.map(item => (
+          <div key={item.key} style={{ cursor: 'pointer' }}>
+            <SidebarItem
+              active={JSON.stringify(item) === JSON.stringify(value)}
+              onClick={handleSelect({ item })}
+            >
+              {item.display}
+            </SidebarItem>
+          </div>
+        ))}
+      </div>
+      <UserMenuItem />
     </div>
   )
 }
--- frontend/src/Sidebar/components/Sidebar/story.js
+++ frontend/src/Sidebar/components/Sidebar/story.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import { Provider } from 'react-redux'
+import { selector } from '../../../UserMenuItem'
 import Sidebar from './index'
 
 const items = [
@@ -19,6 +21,39 @@ const items = [
 ]
 const value = items[0]
 
-storiesOf('Sidebar', module).add('default', () => (
-  <Sidebar items={items} value={value} onChange={action('on-change')} />
-))
+storiesOf('Sidebar', module)
+  .addDecorator(getStory => {
+    const store = {
+      getState: () => ({
+        [selector]: {
+          user: {
+            _id: '5a9aecd5d788d2001e95cbec',
+            email: 'hharnisc@gmail.com',
+            accounts: {
+              github: {
+                avatar: 'https://avatars0.githubusercontent.com/u/1388079?v=4',
+                username: 'hharnisc',
+                name: 'Harrison Harnisch',
+              },
+            },
+            organizationIds: [
+              '5a9aecd997cd03001faff186',
+              '5a9aecd997cd03001faff187',
+              '5a9aecd997cd03001faff188',
+              '5a9aecd997cd03001faff189',
+            ],
+          },
+        },
+      }),
+      subscribe: () => {},
+      dispatch: () => {},
+    }
+    return (
+      <div style={{ width: '18rem', height: '100%' }}>
+        <Provider store={store}>{getStory()}</Provider>
+      </div>
+    )
+  })
+  .add('default', () => (
+    <Sidebar items={items} value={value} onChange={action('on-change')} />
+  ))
