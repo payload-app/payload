a3c55d0f0ac113e9d8c9842a4af776f87311fdf6
Harrison Harnisch <hharnisc@gmail.com>
2018-03-23 09:59:28 -0500

    pass in formated data to file size chart

--- frontend/src/PageRun/components/FileSizeChart/index.js
+++ frontend/src/PageRun/components/FileSizeChart/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { text } from '../../../components/style/color'
 import {
   XYPlot,
   LineSeries,
@@ -8,24 +9,28 @@ import {
   YAxis,
 } from 'react-vis'
 
-const data = [
-  { x: 0, y: 8 },
-  { x: 1, y: 5 },
-  { x: 2, y: 4 },
-  { x: 3, y: 9 },
-  { x: 4, y: 1 },
-  { x: 5, y: 7 },
-  { x: 6, y: 6 },
-  { x: 7, y: 3 },
-  { x: 8, y: 2 },
-  { x: 9, y: 0 },
-]
-export default () => (
-  <XYPlot height={300} width={300}>
+export default ({ fileSizes }) => (
+  <XYPlot height={300} width={800}>
     <VerticalGridLines />
     <HorizontalGridLines />
-    <XAxis />
-    <YAxis />
-    <LineSeries data={data} />
+    <XAxis
+      style={{
+        text: {
+          fontSize: '1.5rem',
+          fill: text,
+        },
+      }}
+    />
+    <YAxis
+      style={{
+        text: {
+          fontSize: '1.5rem',
+          fill: text,
+        },
+      }}
+    />
+    {Object.keys(fileSizes).map(fileName => (
+      <LineSeries key={fileName} data={fileSizes[fileName]} />
+    ))}
   </XYPlot>
 )
