1b2303c11f57646763c0ac728829c0a47acdc1bc
Harrison Harnisch <hharnisc@gmail.com>
2018-04-30 08:27:31 -0500

    use project secrets in user service

--- brigade.js
+++ brigade.js
@@ -202,6 +202,24 @@ const deployUserService = async (event, payload) =>
     valuesFile: 'values.yaml',
     chart: 'payload-service',
     namespace: 'payload',
+    envVars: [
+      {
+        name: 'MONGODB_USERNAME',
+        value: payload.secrets.MONGODB_USERNAME,
+      },
+      {
+        name: 'MONGODB_PASSWORD',
+        value: payload.secrets.MONGODB_PASSWORD,
+      },
+      {
+        name: 'MONGODB_DATABASE',
+        value: payload.secrets.MONGODB_DATABASE,
+      },
+      {
+        name: 'MONGODB_URL',
+        value: payload.secrets.MONGODB_URL,
+      },
+    ],
   })
 
 const deployRunService = async (event, payload) =>
