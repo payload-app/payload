b6fd604701f30b957aab45796241267c77b51e5b
Harrison Harnisch <hharnisc@gmail.com>
2018-06-26 09:18:41 -0500

    stub in startTrial endpoint

--- billing-service/index.js
+++ billing-service/index.js
@@ -1,5 +1,12 @@
-const { router, get } = require('microrouter')
+const { router, get, post } = require('microrouter')
+const { rpc, method } = require('@hharnisc/micro-rpc')
+const startTrial = require('./startTrial')
+
+const rpcHandler = rpc(method('startTrial', startTrial))
 
 const healthHandler = () => 'OK'
 
-module.exports = router(get('/healthz', healthHandler))
+module.exports = router(
+  get('/healthz', healthHandler),
+  post('/rpc', rpcHandler),
+)
