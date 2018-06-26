9db27c154849e6280393910dbfe12ca51047422d
Harrison Harnisch <hharnisc@gmail.com>
2018-06-26 10:15:17 -0500

    implement startTrial billing endpoint

--- billing-service/index.js
+++ billing-service/index.js
@@ -2,6 +2,7 @@ const { promisify } = require('util')
 const { router, get, post } = require('microrouter')
 const { MongoClient } = require('mongodb')
 const { rpc, method } = require('@hharnisc/micro-rpc')
+const RPCClient = require('@hharnisc/micro-rpc-client')
 const startTrial = require('./startTrial')
 
 const promisifiedMongoClient = promisify(MongoClient)
@@ -19,17 +20,48 @@ const init = async handler => {
   const collectionClient = client
     .db(process.env.MONGODB_DATABASE)
     .collection('billing')
-  return handler({ collectionClient })
+  const organizationServiceClient = new RPCClient({
+    url: 'http://organization-service:3000/rpc',
+  })
+  const userServiceClient = new RPCClient({
+    url: 'http://user-service:3000/rpc',
+  })
+  return handler({
+    collectionClient,
+    organizationServiceClient,
+    userServiceClient,
+  })
 }
 
-const rpcHandler = ({ collectionClient }) =>
-  rpc(method('startTrial', startTrial({ collectionClient })))
+const rpcHandler = ({
+  collectionClient,
+  organizationServiceClient,
+  userServiceClient,
+}) =>
+  rpc(
+    method(
+      'startTrial',
+      startTrial({
+        collectionClient,
+        organizationServiceClient,
+        userServiceClient,
+      }),
+    ),
+  )
 
 const healthHandler = () => 'OK'
 
-module.exports = init(({ collectionClient }) =>
-  router(
-    get('/healthz', healthHandler),
-    post('/rpc', rpcHandler({ collectionClient })),
-  ),
+module.exports = init(
+  ({ collectionClient, organizationServiceClient, userServiceClient }) =>
+    router(
+      get('/healthz', healthHandler),
+      post(
+        '/rpc',
+        rpcHandler({
+          collectionClient,
+          organizationServiceClient,
+          userServiceClient,
+        }),
+      ),
+    ),
 )
--- billing-service/startTrial.js
+++ billing-service/startTrial.js
@@ -1,1 +1,60 @@
-module.exports = ({ collectionClient }) => async () => 'startTrial'
+const Joi = require('joi')
+const { validate, parseValidationErrorMessage } = require('./utils')
+const { createError } = require('@hharnisc/micro-rpc')
+
+const schema = Joi.object().keys({
+  organizationId: Joi.string().required(),
+  userId: Joi.string().required(),
+  trialEnd: Joi.date()
+    .min('now')
+    .required(),
+})
+
+module.exports = ({
+  collectionClient,
+  organizationServiceClient,
+  userServiceClient,
+}) => async ({ organizationId, userId, trialEnd }) => {
+  try {
+    await validate({
+      value: {
+        organizationId,
+        trialEnd,
+        userId,
+      },
+      schema,
+    })
+  } catch (error) {
+    throw createError({
+      message: parseValidationErrorMessage({ error }),
+    })
+  }
+  try {
+    // ensure organization exists
+    await organizationServiceClient.call('getOrganization', {
+      id: organizationId,
+    })
+    // ensure user exists
+    const user = await userServiceClient.call('getUser', {
+      id: userId,
+    })
+    if (!user.organizationIds.includes(organizationId)) {
+      throw new Error(
+        `User with id ${userId} does not belong to organization with id ${organizationId}`,
+      )
+    }
+    const { insertedId } = await collectionClient.insertOne({
+      organizationId,
+      userId,
+      trialEnd: new Date(isNaN(trialEnd) ? trialEnd : parseInt(trialEnd, 10)),
+      paymentSourceSet: false,
+    })
+    return {
+      id: insertedId,
+    }
+  } catch (error) {
+    throw createError({
+      message: error.message,
+    })
+  }
+}
