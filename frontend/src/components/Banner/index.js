9880cb735aee2ec849eaea76a35b2f6930f1410c
Harrison Harnisch <hharnisc@gmail.com>
2018-06-30 09:41:59 -0500

    move text into banner component

--- frontend/src/components/Banner/index.js
+++ frontend/src/components/Banner/index.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import Text from '../Text'
+import { invertedBackground, invertedText } from '../style/color'
 
-const Banner = ({ children }) => <div>{children}</div>
+const Banner = ({ children }) => (
+  <div style={{ background: invertedBackground, padding: '1rem' }}>
+    <Text size={2} color={invertedText}>
+      {children}
+    </Text>
+  </div>
+)
 
 export default Banner
