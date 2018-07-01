3fff6483287d7f39aa1cb46228cec09addefaa6d
Harrison Harnisch <hharnisc@gmail.com>
2018-07-01 09:55:59 -0500

    stub in payment form

---
+++ frontend/src/components/PaymentForm/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const PaymentForm = () => <div>PaymentForm</div>
+
+export default PaymentForm
---
+++ frontend/src/components/PaymentForm/story.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import PaymentForm from './index'
+
+storiesOf('PaymentForm', module).add('default', () => <PaymentForm />)
