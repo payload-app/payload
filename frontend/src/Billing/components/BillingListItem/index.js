2dc208741e4805f6b581b2a9c516c7cd2b91875b
Harrison Harnisch <hharnisc@gmail.com>
2018-07-05 11:06:24 -0500

    Use panel in BillingListItem

--- frontend/src/Billing/components/BillingListItem/index.js
+++ frontend/src/Billing/components/BillingListItem/index.js
@@ -1,6 +1,26 @@
 import React from 'react'
-import { Text } from '../../../components'
+import { Text, Panel } from '../../../components'
+import SubscriptionList from '../SubscriptionList'
 
-const BillingListItem = () => <Text>BillingListItem</Text>
+const BillingListItem = ({ billingCustomer, repos, repoOwners }) => (
+  <Panel
+    TitleComponent={
+      <Text size={2.4}>
+        {
+          repoOwners.find(
+            owner =>
+              billingCustomer.ownerId === owner.id &&
+              billingCustomer.ownerType === owner.ownerType,
+          ).name
+        }
+      </Text>
+    }
+  >
+    <SubscriptionList
+      subscriptions={billingCustomer.subscriptions}
+      repos={repos}
+    />
+  </Panel>
+)
 
 export default BillingListItem
--- frontend/src/Billing/components/BillingListItem/story.js
+++ frontend/src/Billing/components/BillingListItem/story.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import BillingListItem from './'
+import billingCustomer from './sampleData'
+import repos from '../sampleRepoData'
+import repoOwners from '../sampleOwnerData'
 
-storiesOf('BillingListItem', module).add('default', () => <BillingListItem />)
+storiesOf('BillingListItem', module).add('default', () => (
+  <BillingListItem
+    billingCustomer={billingCustomer}
+    repos={repos}
+    repoOwners={repoOwners}
+  />
+))
