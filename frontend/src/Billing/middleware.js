8bb7ffe68c02c4947703ee8fa6bd0f6a3a556b95
Harrison Harnisch <hharnisc@gmail.com>
2018-07-07 17:23:11 -0500

    fix loading and init bugs

--- frontend/src/Billing/middleware.js
+++ frontend/src/Billing/middleware.js
@@ -6,26 +6,20 @@ import {
   actionTypes as sidebarActionTypes,
   selector as sidebarSelector,
 } from '../Sidebar'
-import { selector as routingSelector, routes } from '../Routing'
+import {
+  selector as routingSelector,
+  routes,
+  actionTypes as routingActionTypes,
+} from '../Routing'
 
 export default ({ dispatch, getState }) => next => action => {
   next(action)
   switch (action.type) {
-    case sidebarActionTypes.SELECT:
+    case routingActionTypes.EMIT: {
       const state = getState()
       const { repoOwners } = state[sidebarSelector]
       const { route, params } = state[routingSelector]
-      if (route === routes.REPO_LIST) {
-        dispatch(
-          dataFetchActions.fetch({
-            name: 'getBillingCustomer',
-            args: {
-              ownerId: repoOwners[action.selection].id,
-              ownerType: repoOwners[action.selection].ownerType,
-            },
-          }),
-        )
-      } else if (
+      if (
         route === routes.OWNER_SETTINGS &&
         params.settingsType === 'billing'
       ) {
@@ -42,6 +36,24 @@ export default ({ dispatch, getState }) => next => action => {
         )
       }
       break
+    }
+    case sidebarActionTypes.SELECT: {
+      const state = getState()
+      const { repoOwners } = state[sidebarSelector]
+      const { route } = state[routingSelector]
+      if (route === routes.REPO_LIST) {
+        dispatch(
+          dataFetchActions.fetch({
+            name: 'getBillingCustomer',
+            args: {
+              ownerId: repoOwners[action.selection].id,
+              ownerType: repoOwners[action.selection].ownerType,
+            },
+          }),
+        )
+      }
+      break
+    }
     case `setPaymentSource_${dataFetchActionTypes.FETCH_SUCCESS}`:
       dispatch(
         dataFetchActions.fetch({
--- frontend/src/RepoList/middleware.js
+++ frontend/src/RepoList/middleware.js
@@ -1,21 +1,23 @@
 import { actions as dataFetchActions } from '@hharnisc/async-data-fetch'
-import { actionTypes as sidebarActionTypes, selector } from '../Sidebar'
-import { selector as routingSelector, routes } from '../Routing'
+import {
+  selector as routingSelector,
+  actionTypes as routerActionTypes,
+  routes,
+} from '../Routing'
 
 export default ({ dispatch, getState }) => next => action => {
   next(action)
   switch (action.type) {
-    case sidebarActionTypes.SELECT:
+    case routerActionTypes.EMIT:
       const state = getState()
-      const { repoOwners } = state[selector]
-      const { route } = state[routingSelector]
+      const { route, params } = state[routingSelector]
       if (route === routes.REPO_LIST) {
         dispatch(
           dataFetchActions.fetch({
             name: 'repos',
             args: {
-              name: repoOwners[action.selection].name,
-              ownerType: repoOwners[action.selection].ownerType,
+              name: params.owner,
+              ownerType: params.ownerType,
             },
           }),
         )
