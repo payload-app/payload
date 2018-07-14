97f6bb14820cc0e4580111c3c5818b402dba535b
Harrison Harnisch <hharnisc@gmail.com>
2018-07-14 08:27:28 -0500

    stub in confirm dialog

---
+++ frontend/src/components/ConfirmDialog/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Text from '../Text'
+
+const ConfirmDialog = () => <Text>ConfirmDialog</Text>
+
+export default ConfirmDialog
---
+++ frontend/src/components/ConfirmDialog/story.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import ConfirmDialog from './index'
+
+storiesOf('ConfirmDialog', module).add('default', () => <ConfirmDialog />)
