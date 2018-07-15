39835f19180561cebfc65317fbd13d6256e71eb2
Harrison Harnisch <hharnisc@gmail.com>
2018-07-15 08:23:10 -0500

    add title and text components

--- frontend/src/components/ConfirmDialog/index.js
+++ frontend/src/components/ConfirmDialog/index.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import InvertedPanel from '../InvertedPanel'
 import Text from '../Text'
+import { invertedText } from '../style/color'
 
-const ConfirmDialog = () => <Text>ConfirmDialog</Text>
+const ConfirmDialog = ({ children, title }) => (
+  <InvertedPanel>
+    <div>
+      <Text color={invertedText} size={2}>
+        {title}
+      </Text>
+    </div>
+    <div>
+      <Text color={invertedText}>{children}</Text>
+    </div>
+  </InvertedPanel>
+)
 
 export default ConfirmDialog
--- frontend/src/components/ConfirmDialog/story.js
+++ frontend/src/components/ConfirmDialog/story.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import Text from '../Text'
 import ConfirmDialog from './index'
 
-storiesOf('ConfirmDialog', module).add('default', () => <ConfirmDialog />)
+storiesOf('ConfirmDialog', module).add('default', () => (
+  <ConfirmDialog title={'Are You Sure?'}>
+    Are you sure this is the way to go? It cannot be undone.
+  </ConfirmDialog>
+))
