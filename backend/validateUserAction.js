5c4c73b7250e4bf597e7cac41bad419f10aa4d6d
Harrison Harnisch <hharnisc@gmail.com>
2018-07-27 11:39:36 -0500

    validate user checks user type resources

--- backend/validateUserAction.js
+++ backend/validateUserAction.js
@@ -1,22 +1,39 @@
-const { getUserFromSession } = require('./utils')
+const {
+  getUserFromSession,
+  parseGithubUsernameFromSession,
+} = require('./utils')
 
 module.exports = async ({
   session,
   name,
   type,
+  ownerType,
   id,
   organizationServiceClient,
 }) => {
   const user = getUserFromSession({ session })
-  let organizationId = id
-  if (!id) {
-    const result = await organizationServiceClient.call('getOrganization', {
-      name,
-      type,
-    })
-    organizationId = result._id
-  }
-  if (!organizationId || !user.organizationIds.includes(organizationId)) {
-    throw new Error('User is not part of organization')
+  if (ownerType === 'user') {
+    if (type === 'github') {
+      const githubUsername = parseGithubUsernameFromSession({ session })
+      if (githubUsername !== name) {
+        throw new Error('User is not logged in user')
+      }
+    } else {
+      throw new Error(
+        'Need to implement other types for validating user actions',
+      )
+    }
+  } else {
+    let organizationId = id
+    if (!id) {
+      const result = await organizationServiceClient.call('getOrganization', {
+        name,
+        type,
+      })
+      organizationId = result._id
+    }
+    if (!organizationId || !user.organizationIds.includes(organizationId)) {
+      throw new Error('User is not part of organization')
+    }
   }
 }
